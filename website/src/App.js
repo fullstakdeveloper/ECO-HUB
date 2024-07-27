b4249b295b7256423d87fa01d253c4e1a7a7d03f
Ruiqi Tang <tangricky001@gmail.com>
2024-07-27 17:25:32 -0400

    simple input

--- website/src/App.js
+++ website/src/App.js
@@ -1,10 +1,23 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  // Create a state variable to store the input value
+
+  const [inputValue, setInputValue] = useState('');
+
+  const handleInputChange = (event) => 
+  {
+    setInputValue(event.target.value); // new input value
+  };
+
   return (
     <div className="App">
-
+      <input value={inputValue} // Set the input value from the state
+        onChange={handleInputChange}
+      />
+      <p>Stored value: {inputValue}</p>
     </div>
   );
 }
