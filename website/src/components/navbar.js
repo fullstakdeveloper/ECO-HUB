8f2e93298f700eb56052629f0288c158980bdc1a
Ruiqi Tang <tangricky001@gmail.com>
2024-07-28 08:31:26 -0400

    good one

--- website/src/components/navbar.js
+++ website/src/components/navbar.js
@@ -4,15 +4,13 @@ import LOGO from '../Assets/ecohub_new.png';
 import App from "../App.js";
 
 
-
-
 function Navbar() {
   return (
     <div className="navbar">
       <img src={LOGO} alt="Logo" />
       <div>Home</div>
-      <div>Shop</div>
       <div>About</div>
+      <div>Shop</div>
     </div>
   );
 }
