4dc5aaeff53c2fbc2abee8da62f45df0bb08d52c
Ruiqi Tang <tangricky001@gmail.com>
2024-07-28 09:44:19 -0400

    I finished the shop

--- website/src/components/shop.js
+++ website/src/components/shop.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import Navbar from './navbar';
+import './shop.css';
+import trophyIcon from '../Assets/trophy_icon.png'; // Add the correct path to your trophy icon image
 
 function Shop() {
   return (
     <div className="shop">
       <Navbar/>
-      <h1>Shop at EcoHub</h1>
+      <div className="shop-content">
+        <h1>Welcome to the Shop!</h1>
+        <div className="score-container">
+          <div className="score">
+            <div className="score-shape">
+              <img src={trophyIcon} alt="Trophy Icon" className="trophy-icon" />
+            </div>
+            <span>56</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
