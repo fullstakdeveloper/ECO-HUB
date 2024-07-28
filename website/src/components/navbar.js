66f3c9a34935225583e61bd8dcccaed1f546fc05
fullstakdeveloper <pupru.edu2k7@gmail.com>
2024-07-28 00:30:47 -0400

    about

--- website/src/components/navbar.js
+++ website/src/components/navbar.js
@@ -7,9 +7,9 @@ function Navbar() {
     return (
         <div className="navbar">
             <img src={LOGO} alt="Logo" />
-            <div>Home</div>
-            <div>About</div>
-            <div>Shop</div>
+            <div><a href = "../App.js">Home</a></div>
+            <div><a>About</a></div>
+            <div><a>Shop</a></div>
         </div>
     );
 }
