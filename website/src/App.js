349b7ec5b830ef3050e5158d6adf7c581b77edf0
Ruiqi Tang <tangricky001@gmail.com>
2024-07-27 21:59:07 -0400

    6 images

--- website/src/App.js
+++ website/src/App.js
@@ -1,22 +1,38 @@
 // app.js
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Image from './components/image';
 import './App.css';
-import LOGO from './logo.svg';
+import REACT_LOGO from './Assets/logo.svg';
+import BOTTLE from './Assets/bottle.jpeg';
+import BRICKS from './Assets/bricks.jpg';
+import SQUARES from './Assets/8_squares.jpg';
+import GREENLIGHT from './Assets/green_light.jpg';
+import MICROSOFT from './Assets/microsoft.jpg';
+import LOGO from './Assets/ecohub_logo.png';
 
 function App() {
-  const container = document.getElementById('root');
-  const root = ReactDOM.createRoot(container);
-  root.render(<Image m = {LOGO} />);
-
   return (
     <div className="App">
-      
+
+      <img src = {LOGO}></img>
+      <div className = "acts">
+        <Image m={REACT_LOGO} event = "Jashan" />
+        <Image m={BOTTLE} event = "Ricky"/>
+        <Image m={BRICKS} event = "Jason"/>
+
+        <br></br>
+
+        <Image m={SQUARES} event = "Vadant"/>
+        <Image m={GREENLIGHT} event = "Enkai"/>
+        <Image m={MICROSOFT} event = "Lol"/>
+      </div>
     </div>
   );
 }
 
-
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+root.render(<App />);
 
 export default App;
--- website/src/components/image.js
+++ website/src/components/image.js
@@ -1,21 +1,15 @@
 // image.js
-import "./image.css";
-
+import './image.css';
 import React from 'react';
 
-function Image(props) 
-{
-    return (
-        <div className = "state">
-            
-            <h1>Event</h1>
-            <img src = {props.m}></img>
-            <div id = "prog"></div>
-
-            
-
-        </div>
-    );
+function Image(props) {
+  return (
+    <div className="state">
+      <h1>{props.event}</h1>
+      <img src={props.m} alt="Event" />
+      <div id="prog"></div>
+    </div>
+  );
 }
 
 export default Image;
