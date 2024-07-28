5c2c9368ad1841559dce9417593a657ad3eff2d0
fullstakdeveloper <pupru.edu2k7@gmail.com>
2024-07-28 12:45:30 -0400

    the about me

--- website/src/components/about.js
+++ website/src/components/about.js
@@ -13,14 +13,10 @@ function About() {
       <div className="content">
       <img src = {BAH} id = "jashan"></img>
       <div id = "cont">
-        <h2>About EcoHub</h2>
-        <p id = "broke">
-          Eco Hub is an activity based game where people all over the world can participate to entertain 
-          themselves or caring for their home environment or the environment. It was created for people all 
-          over the world are waiting for upcoming games / video games and waste time into nothing to try reward full. 
-          Instead, what they can do is playing our activities and engage with friendly ecosystem activities so they could enjoy 
-          and the world to enjoy.
-        </p>
+        <h2 id = "text">ECO-HUB</h2>
+        <div id = "broke">
+          A way to help the environment with prizes.
+        </div>
       </div>
       </div>
     </div>
